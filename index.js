deed820dd405edf09729b039b7247dd8926ff91d
Lukas <lukkotecki@gmail.com>
2024-06-05 22:29:10 +0200

    JavaScript basic functionality finiched

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-
-
+console.clear()
 
 const question = document.getElementsByClassName('question')
 
-for(q in question){
-    console.log(q)
-}
-
 console.log(question)
+
+
+
+for(let i=0; i<question.length; i++){
+    console.log(question[i])
+
+    question[i].addEventListener('click', ()=>{
+
+        console.log( question[i].parentNode.childNodes[3] )
+
+        if(question[i].childNodes[3].classList[0] === "plus-icon" ){
+            question[i].childNodes[3].src = 'assets/images/icon-minus.svg'
+            question[i].childNodes[3].classList.toggle('plus-icon')
+            question[i].parentNode.childNodes[3].style.display = 'initial'
+        }else{
+            question[i].childNodes[3].src = 'assets/images/icon-plus.svg'
+            question[i].childNodes[3].classList.toggle('plus-icon')
+            question[i].parentNode.childNodes[3].style.display = 'none'
+        }
+    })
+}
