dfa67bd4ccf3780a602c04705a054a4018ba0183
Lukas <lukkotecki@gmail.com>
2024-06-08 15:14:38 +0200

    update

--- index.js
+++ index.js
@@ -1,38 +1,10 @@
-
-
-
-
-
-document.addEventListener('animationstart', function (e) {
-    if (e.animationName === 'fade-in') {
-        e.target.classList.add('did-fade-in');
-    }
-  });
-  
-  document.addEventListener('animationend', function (e) {
-    if (e.animationName === 'fade-out') {
-        e.target.classList.remove('did-fade-in');
-     }
-  });
-
-
-
-
-
-console.clear()
-
+// console.clear()
 const question = document.getElementsByClassName('question')
 
-console.log(question)
-
-
-
 for(let i=0; i<question.length; i++){
-    console.log(question[i])
+    // console.log(question[i])
 
     question[i].addEventListener('click', ()=>{
-
-        console.log( question[i].parentNode.childNodes[3] )
 
         if(question[i].childNodes[3].classList[0] === "plus-icon" ){
             question[i].childNodes[3].src = 'assets/images/icon-minus.svg'
@@ -53,8 +25,3 @@ for(let i=0; i<question.length; i++){
         }
     })
 }
-
-
-// opacity: 0;
-//     height: 0;
-//     overflow: hidden;
